f40dc883dc64ce6ceb04e00b49f30c06f5a08aef
Elizabethibarr <rivasibarra680@gmail.com>
2025-02-15 15:33:11 -0600

    agregando practicas de laboratorio

---
+++ mini_introducción/app.js
@@ -0,0 +1,27 @@
+
+
+const Users = [
+    {
+       fullname :"Daniela Ibarra",
+       email : "rivasibarra680@gmail.com",
+       password : "ibarra 16",
+       age : 17
+
+    },
+    
+    {
+
+     fullname : "Karla Morales",
+     email : "karla@gmail.com",
+     password : "123445",
+     age : 20
+   
+    }
+]
+
+for(let item of Users){
+  console.log(item.fullname);
+  
+}
+
+ 
